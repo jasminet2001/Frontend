e0aa95f45e729628abe34086c7800ce38958c250
arshia <arshia123455@gmail.com>
2022-04-19 09:32:15 +0430

    signup and login pages merged

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <login-component v-if="isAuthenticated"/>
+  <signup-component v-else />
+</template>
+
+<script>
+import LoginComponent from "@/components/login/LoginComponent";
+import SignupComponent from "@/components/signup/SignupComponent";
+export default {
+  components: {
+    SignupComponent,
+    LoginComponent
+  },
+  data () {
+    return {
+      isAuthenticated: true
+    }
+  }
+}
+</script>
+<style>
+@import "assets/main.css";
+</style>
---
+++ src/components/login/LoginPic.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-row align="center" justify="center">
+      <h1 style="color: white;">ورود</h1>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <v-img src="https://svgshare.com/i/g2T.svg" class="img">
+      </v-img>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.img{
+  margin-top: 4em;
+}
+</style>
