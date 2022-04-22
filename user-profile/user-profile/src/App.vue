55da7ebc5dc2fbdd461b54728de2db6da7f5c086
JasmineT <jasminesr2001@gmail.com>
2022-04-23 01:01:47 +0430

    dashboard

--- user-profile/user-profile/src/App.vue
+++ user-profile/user-profile/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <v-app>
     <navbar-item/>
+    <dashboard-item class="mr-16"/>
   </v-app>
 </template>
 
 <script>
+import DashboardItem from './components/dashboard-item.vue'
 import navbarItem from './components/navbar-item.vue'
 
 export default {
   components: { 
-    navbarItem 
+    navbarItem,
+    DashboardItem 
   },
   name: 'App',
 
---
+++ user-profile/user-profile/src/components/dashboard-item.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-container>
+        <v-row>
+            <h1>داشبورد</h1>
+        </v-row>
+        <v-row>
+            <!-- how to put the username -->
+            <h3>خوش آمدید </h3>
+        </v-row>
+        <v-row>
+            <v-card
+            elevation="2"
+            outlined
+            shaped
+            >
+            </v-card>
+
+            <v-card
+            elevation="2"
+            outlined
+            shaped
+            >
+            </v-card>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'dashboard-item',
+}
+</script>
