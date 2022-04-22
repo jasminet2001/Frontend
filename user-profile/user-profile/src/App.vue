29b8b068baf72c55f7f8f1e0f03f91917a48eba1
JasmineT <jasminesr2001@gmail.com>
2022-04-23 00:28:12 +0430

    navbar component

--- user-profile/user-profile/src/App.vue
+++ user-profile/user-profile/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <v-app>
-
+    <navbar-item/>
   </v-app>
 </template>
 
 <script>
+import navbarItem from './components/navbar-item.vue'
 
 export default {
+  components: { 
+    navbarItem 
+  },
   name: 'App',
 
-  data: () => ({
-    //
-  }),
-};
+}
 </script>
