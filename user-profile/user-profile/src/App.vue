374b08b010dcf9b2c4a01b78e07a687b38c8a1f7
JasmineT <jasminesr2001@gmail.com>
2022-04-23 03:35:12 +0430

    edit-profile

--- user-profile/user-profile/src/App.vue
+++ user-profile/user-profile/src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <v-app>
     <navbar-item/>
-    <dashboard-item class="mr-16"/>
+    <div v-if="dashboard">
+      <dashboard-item class="mr-16"/>
+    </div>
+    <div v-else>
+      <edit-profile/>
+    </div>
   </v-app>
 </template>
 
 <script>
 import DashboardItem from './components/dashboard-item.vue'
+import EditProfile from './components/edit-profile.vue';
 import navbarItem from './components/navbar-item.vue'
 
 export default {
+  name: 'App',
   components: { 
     navbarItem,
-    DashboardItem 
+    DashboardItem,
+    EditProfile 
   },
-  name: 'App',
-
+    data() {
+      return {
+          dashboard: false,
+      };
+    }
 }
 </script>
---
+++ user-profile/user-profile/src/components/edit-profile.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app>
+                <h1 class="mr-16">Edit</h1>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'edit-profile',
+}
+</script>
+
