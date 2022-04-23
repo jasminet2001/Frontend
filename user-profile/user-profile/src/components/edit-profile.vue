66de4e4f88bbd5b07df5335d1ee79196a41fcc91
JasmineT <jasminesr2001@gmail.com>
2022-04-23 16:37:31 +0430

    form

--- user-profile/user-profile/src/components/edit-profile.vue
+++ user-profile/user-profile/src/components/edit-profile.vue
@@ -1,6 +1,22 @@
 <template>
-    <v-app>
-                <h1 class="mr-16">Edit</h1>
+    <v-app class="mx-6">
+        <v-row class="ma-6">
+            <v-col>
+                <h1 class="mr-16">پروفایل</h1>
+            </v-col>
+            <v-col>
+                <v-icon>mdi-chevron-left</v-icon>
+            </v-col>
+            <v-col>
+                <h1>ویرایش پروفایل</h1>
+            </v-col>
+        </v-row>
+
+        <v-card>
+            <v-form>
+
+            </v-form>
+        </v-card>
     </v-app>
 </template>
 
