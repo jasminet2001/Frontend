ce6afc9e375ac48f62b4d396c08edd4f7bbf4a5f
ali26att <ali.live26.aa@gmail.com>
2022-04-24 09:00:50 +0430

    creating ad_request branch

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app style="background-color: #353B51;">
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        
+        rules: {
+          
+        },
+      }
+    },
+  }
+</script>
+<style scoped>
+  
+</style>
