ad7d6d89ae8716e2419e489f2c26f6d30c4aa95f
JasmineT <jasminesr2001@gmail.com>
2022-04-29 02:00:14 +0430

    navbar

---
+++ homepage/homepage/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- why doesnt it take up the whole space? -->
+  <v-container fluid>
+    <v-layout row>
+      <v-flex>
+        <navbar-menu/>
+      </v-flex>
+  </v-layout>
+  <v-layout row>
+    <!-- <v-flex
+    :style="{'background-image': `url(${require(image)})`,
+    height:200,}">
+    </v-flex> -->
+    <v-flex>
+      
+    </v-flex>
+  </v-layout>
+  </v-container>
+
+</template>
+
+<script>
+import navbarMenu from './components/navbar-menu.vue';
+
+
+export default {
+  components: { navbarMenu },
+  name: 'App',
+
+
+  data: () => ({
+    image: "@/assets/top_pic 1.jpg",
+  }),
+};
+</script>
---
+++ homepage/homepage/src/components/navbar-menu.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-app>
+        <!-- why is the menu rounded? -->
+        <v-card>
+            <v-toolbar
+            color="#000930"
+            dark
+            flat>
+                <v-tabs
+                v-model="tab"
+                align-with-title>
+                    <v-tab>
+                        <v-avatar
+                        color="primary"
+                        size="36"
+                        ></v-avatar>
+                    </v-tab>
+                    <v-tabs-slider color="white"></v-tabs-slider>
+                    <v-tab>صفحه اصلی</v-tab>
+                    <v-tab>دسته بندی</v-tab>
+                    <v-tab>ثبت شرکت</v-tab>
+                    <v-tab>تماس با ما</v-tab>
+                    <v-spacer></v-spacer>
+                    <v-tab>
+                        <!-- how to create this btn -->
+                        <v-btn
+                        rounded
+                        outlined
+                        color="white"
+                        >
+                            ورود
+                        </v-btn>
+                    </v-tab>
+                </v-tabs>
+            </v-toolbar>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+export default({
+    name: 'navbar-menu',
+    data(){
+        return{
+            tab:null,
+        }
+    },
+})
+</script>
