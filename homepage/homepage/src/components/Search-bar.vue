aa7af19adafe76bcad37168e2057dd6c88a24aeb
JasmineT <jasminesr2001@gmail.com>
2022-04-29 11:15:33 +0430

    search

--- homepage/homepage/src/components/Search-bar.vue
+++ homepage/homepage/src/components/Search-bar.vue
@@ -1,3 +1,11 @@
 <template>
-    
+    <v-app>
+        <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-text-field
+            label="Search"
+            solo
+          ></v-text-field>
+    </v-app>
 </template>
