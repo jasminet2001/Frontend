f57ec6aead520e98869e30b6c09592b0d7ee1280
arshia <arshia123455@gmail.com>
2022-04-29 12:14:05 +0430

    background image added

--- homepage/homepage/src/App.vue
+++ homepage/homepage/src/App.vue
@@ -1,31 +1,24 @@
 <template>
   <!-- why doesnt it take up the whole space? -->
-  <v-container fluid>
-    <v-layout row>
-      <v-flex>
-        <navbar-menu/>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex
-      style="
-      background: url('@/assets/top-pic.jpg') no-repeat center fixed !important; 
-      background-size: cover;">
-      </v-flex>
-      <v-flex>
-        
-      </v-flex>
-    </v-layout>
-  </v-container>
+<!--  <v-container fluid>-->
+<!--    <v-layout row>-->
+<!--      <v-flex>-->
+<!--        <navbar-menu/>-->
+<!--      </v-flex>-->
+<!--    </v-layout>-->
+<!--  </v-container>-->
+  <v-main>
+    <search-bar />
+  </v-main>
 
 </template>
 
 <script>
-import navbarMenu from './components/navbar-menu.vue';
+import SearchBar from "@/components/Search-bar";
 
 
 export default {
-  components: { navbarMenu },
+  components: {SearchBar},
   name: 'App',
 
 
--- homepage/homepage/src/components/Search-bar.vue
+++ homepage/homepage/src/components/Search-bar.vue
@@ -1,11 +1,21 @@
 <template>
-    <v-app>
+    <div class="main-div">
         <v-btn icon>
             <v-icon>mdi-magnify</v-icon>
         </v-btn>
         <v-text-field
             label="Search"
             solo
+            rounded
+            style="width: 80%; margin: 0 auto"
           ></v-text-field>
-    </v-app>
+    </div>
 </template>
+
+<style scoped>
+.main-div{
+  background-image: url("../assets/top-pic.jpg") !important;
+  background-size: cover;
+  height: 626px;
+}
+</style>
