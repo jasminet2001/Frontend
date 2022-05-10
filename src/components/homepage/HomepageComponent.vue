b80916caa73fc508b95b543ea417f6039f174361
Sepehr Mirshahi <sepehr.mirshahi@outlook.com>
2022-05-10 21:45:19 +0430

    Router

---
+++ src/components/homepage/HomepageComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-app style="background-color: white">
+      <app-bar />
+      <search-bar />
+      <top-carousel />
+      <category-ads />
+      <costumers-slide-group />
+      <register-company />
+      <homepage-footer />
+    </v-app>
+</template>
+
+<script>
+import SearchBar from './SearchBar';
+import AppBar from './AppBar.vue';
+import TopCarousel from './TopCarousel.vue';
+import HomepageFooter from "./HomepageFooter";
+import CostumersSlideGroup from "./CostumersSlideGroup";
+import RegisterCompany from "./RegisterCompany";
+import CategoryAds from "./CategoryAds";
+
+export default {
+  name: "HomepageComponent",
+  components: {
+    RegisterCompany,
+    CostumersSlideGroup,
+    AppBar,
+    SearchBar,
+    TopCarousel,
+    HomepageFooter,
+    CategoryAds,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
