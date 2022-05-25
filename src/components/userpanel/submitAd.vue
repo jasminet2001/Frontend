27fbf13d1dd78c54df777bfb8c2c56b3cd6337ab
arshia <arshia123455@gmail.com>
2022-05-25 17:28:11 +0430

    fix sumbit ads responsive

--- src/components/userpanel/submitAd.vue
+++ src/components/userpanel/submitAd.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- desktop -->
     <v-app v-if="this.$vuetify.breakpoint.mdAndUp">
+      <SidebarNavigation/>
       <v-sheet color="transparent" elevation="0" class="pa-8">
-        <SidebarNavigation/>
         <h2 class="ma-5">ثبت آگهی</h2>
         <v-card
         class="card-desktop"
@@ -18,7 +18,7 @@
             <v-form class="px-3" v-model="submitForm">
               <v-row class="pa-3">
                 <v-col cols="6"
-                class="px-8 col">        
+                class="px-8 col">
                     <v-row class="row">
                         <p>عنوان آگهی</p>
                         <v-text-field
@@ -73,71 +73,74 @@
 
     <!-- responsive -->
     <v-app v-else>
-        <SidebarNavigation/>
+      <SidebarNavigation/>
+      <v-sheet class="pa-8" color="transparent" elevation="0">
         <h2 class="mb-5">ثبت آگهی</h2>
         <v-card
-        class="card-res"
-        width="90vw"
-        height="85%"
-        elevation="4"
-        raised
-        rounded
-        outlined
+            class="card-res"
+            width="90vw"
+            height="85%"
+            elevation="4"
+            raised
+            rounded
+            outlined
         >
-            <v-card-text>
-                <v-form class="px-3">
-                    <v-row class="d-flex flex-column">
-                        <v-col cols="12"
-                        class="px-8 col">
-                            <v-row class="row">
-                                <v-text-field
-                                label="عنوان آگهی"
-                                v-model="name"
-                                outlined>
-                                </v-text-field>
-                            </v-row>
+          <v-card-text>
+            <v-form class="px-3">
+              <v-row class="d-flex flex-column">
+                <v-col cols="12"
+                       class="px-8 col">
+                  <v-row class="row">
+                    <p>توضیحات آگهی</p>
+                    <v-text-field
+                        label="عنوان آگهی"
+                        v-model="name"
+                        outlined>
+                    </v-text-field>
+                  </v-row>
 
-                            <v-row class="row">
-                                <p>توضیحات آگهی</p>
-                                <v-textarea
-                                clearable
-                                outlined
-                                shaped
-                                label="توضیحات آگهی">
-                            </v-textarea>
-                            </v-row>
+                  <v-row class="row">
+                    <p>توضیحات آگهی</p>
+                    <v-textarea
+                        clearable
+                        outlined
+                        shaped
+                        label="توضیحات آگهی">
+                    </v-textarea>
+                  </v-row>
 
-                            <!-- save button -->
-                            <v-btn
-                            class="btn-mobile"
-                            elevation="4"
-                            large
-                            outlined
-                            raised
-                            text
-                            style="background-color: #3751FF;
+                  <!-- save button -->
+                  <v-btn
+                      class="btn-mobile"
+                      elevation="4"
+                      large
+                      outlined
+                      raised
+                      text
+                      style="background-color: #3751FF;
                             color:white;"
-                            @click="submit"
-                            >
-                                ذخیره تغییرات
-                            </v-btn>
+                      @click="submit"
+                  >
+                    ذخیره تغییرات
+                  </v-btn>
 
-                            <!-- clear button -->
-                            <v-btn
-                            class="btn-mobile"
-                            color="#3751FF"
-                            elevation="4"
-                            outlined
-                            large
-                            raised
-                            @click="clear"
-                            >لغو
-                            </v-btn>
-                        </v-col>
-                    </v-row>
-                </v-form>
-            </v-card-text>
+                  <!-- clear button -->
+                  <v-btn
+                      class="btn-mobile"
+                      color="#3751FF"
+                      elevation="4"
+                      outlined
+                      large
+                      raised
+                      @click="clear"
+                  >لغو
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card-text>
         </v-card>
+      </v-sheet>
     </v-app>
 </template>
 
