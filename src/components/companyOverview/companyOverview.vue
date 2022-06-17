8f57a8f5dce7c84d0ab4a52a0747e55056df7938
arshia <arshia123455@gmail.com>
2022-06-17 21:05:18 +0430

    company overview page

---
+++ src/components/companyOverview/companyOverview.vue
@@ -0,0 +1,116 @@
+<template>
+<v-app>
+  <app-bar></app-bar>
+  <v-sheet class="py-10 px-15">
+    <v-row>
+      <header-component />
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="4" lg="3" xl="3" class="ma-0">
+        <summary-side-bar />
+      </v-col>
+      <v-spacer />
+      <v-col cols="12" md="8" lg="9" xl="9">
+        <description-component class="mb-4"/>
+        <comments-component />
+      </v-col>
+    </v-row>
+  </v-sheet>
+  <homepage-footer />
+</v-app>
+</template>
+
+<script>
+
+import appBar from "../homepage/AppBar";
+import summarySideBar from "./summarySideBar"
+import headerComponent from "./headerComponent"
+import descriptionComponent from "./descriptionComponent"
+import homepageFooter from "../homepage/HomepageFooter";
+import CommentsComponent from "@/components/companyOverview/commentsComponent";
+
+export default {
+  components: {
+    CommentsComponent,
+    appBar,
+    summarySideBar,
+    headerComponent,
+    descriptionComponent,
+    homepageFooter
+  },
+  data () {
+    return {
+      adInfo: '',
+      categories: '',
+      infoForSummary: [
+
+      ]
+    }
+  },
+  methods:{
+    // async ad(id=this.$route.params.id){
+    //   var axios = require('axios');
+    //   var FormData = require('form-data');
+    //   var data = new FormData();
+    //   data.append('id', id);
+    //
+    //   var config = {
+    //     method: 'post',
+    //     url: 'http://localhost:8000/api/company/search',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //     },
+    //     data : data
+    //   };
+    //   let that = this;
+    //   await axios(config)
+    //       .then(function (response) {
+    //         that.adInfo=response.data
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error);
+    //       });
+    // },
+    // categoryFinder(){
+    //   var axios = require('axios');
+    //   var FormData = require('form-data');
+    //   var data = new FormData();
+    //   var config = {
+    //     method: 'get',
+    //     url: 'http://localhost:8000/api/categories',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //     },
+    //     data : data
+    //   };
+    //   let that = this;
+    //   axios(config)
+    //       .then(function (response) {
+    //         that.categories=response.data.categories;
+    //         that.categories.unshift({id:'',name:'همه'});
+    //       })
+    // },
+    // categoryName(id){
+    //   for (let i=0;i<this.categories.length;i++){
+    //     if (this.categories[i].id===id){
+    //       return this.categories[i].name;
+    //     }
+    //   }
+    // }
+  },
+  // async beforeRouteUpdate(to){
+  //   await this.ad(to.params.id)
+  // },
+  // async beforeMount() {
+  //   await this.categoryFinder()
+  //   await this.ad()
+  // }
+}
+</script>
+
+<style scoped>
+.btn{
+  width: 100%;
+  margin-top: 1rem;
+}
+</style>
---
+++ src/components/companyOverview/headerComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-card elevation="2" class="main px-8 pb-5">
+    <v-card class="img">
+      <v-img elevation="5">
+        <v-icon size="6rem">mdi-factory</v-icon>
+      </v-img>
+    </v-card>
+    <div>
+      <v-row>
+        <v-spacer />
+        <v-col cols="3" align="center">
+          <h1>
+            {{ companyName }}
+          </h1>
+        </v-col>
+        <v-col cols="3" align="center">
+          <v-rating
+              :empty-icon="ImdiStarOutline"
+              :full-icon="ImdiStar"
+              :half-icon="ImdiStarHalf"
+              hover
+              length="5"
+              readonly
+              value="3"
+              color="yellow"
+              background-color="gray"
+              style="direction: ltr;"
+          ></v-rating>
+        </v-col>
+        <v-col cols="3">
+          <v-chip color="#ffeb3b">
+            {{ companyCategory }}
+          </v-chip>
+        </v-col>
+        <v-col cols="3">
+          <v-icon small>
+            {{ ImdiPin }}
+          </v-icon>
+          {{ companyLocation }}
+        </v-col>
+      </v-row>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import { mdiMapMarker, mdiStar, mdiStarHalfFull, mdiStarOutline } from '@mdi/js'
+export default {
+  data () {
+    return {
+      companyName: 'کاله',
+      companyCategory: 'دسته بندی',
+      companyLocation: 'تهران',
+      ImdiPin: mdiMapMarker,
+      ImdiStar: mdiStar,
+      ImdiStarHalf: mdiStarHalfFull,
+      ImdiStarOutline: mdiStarOutline
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main{
+  width: 100%;
+  margin-top: 3rem;
+  padding-top: 5rem;
+}
+.img{
+  position: absolute;
+  right: 3%;
+  top: -3rem;
+  width: 100px;
+}
+</style>
