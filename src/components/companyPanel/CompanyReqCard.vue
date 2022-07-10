3dfda2cbc56d451a85fad551d29b42c96e8f84d2
JasmineT <jasminesr2001@gmail.com>
2022-07-10 12:52:19 +0430

    requests

--- src/components/companyPanel/CompanyReqCard.vue
+++ src/components/companyPanel/CompanyReqCard.vue
@@ -17,6 +17,46 @@
         {{ ad.Status }}
       </v-row>
     </v-card-text>
+    <v-card-actions class="justify-center align-center mt-1">
+      <!-- desktop -->
+      <v-row class="mr-16" v-if="this.$vuetify.breakpoint.mdAndUp">
+        <v-col cols="6">
+          <v-btn class="btn"
+          color="success">
+            <v-icon></v-icon>
+            <span>پذیرفتن</span>
+          </v-btn>
+        </v-col>
+        <v-col cols="6">
+          <v-btn class="btn"
+          color="error">
+            <v-icon></v-icon>
+            <span>رد کردن</span>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <!-- responsive -->
+      <v-col v-else cols="12" style="margin-right: 4em;">
+        <v-row class="">
+          <v-col cols="6">
+            <v-btn class="btn"
+            color="success">
+              <v-icon></v-icon>
+              <span>پذیرفتن</span>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6">
+            <v-btn class="btn"
+            color="error">
+              <v-icon></v-icon>
+              <span>رد کردن</span>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-card-actions>
     <v-card-actions class="justify-center align-center mt-1 mb-0">
       <v-btn width="40%" text color="white">
         <v-icon color="white" class="ml-2" style="bottom: 1px">{{ ImdiInfo }}</v-icon>
@@ -48,5 +88,4 @@ export default {
 background: rgb(161,156,251);
 background: linear-gradient(90deg, rgba(161,156,251,1) 0%, rgba(99,99,245,1) 68%);
 }
-
 </style>
