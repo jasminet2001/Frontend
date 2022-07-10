7fffa5ff3666c38c562b703da38c426c8f2834d2
JasmineT <jasminesr2001@gmail.com>
2022-07-10 20:18:56 +0430

    about us

--- src/components/aboutUs/aboutCard.vue
+++ src/components/aboutUs/aboutCard.vue
@@ -1,10 +1,10 @@
 <template>
 <section class="my-16 py-5">
 
-  <v-card elevation="10" :class="'mb-14 pb-5 pt-16 '+(right?'pr-10 pl-3':'pr-3 pl-10')" shaped style="background: #9FA2B4;">
+  <v-card elevation="10" :class="'mb-14 pb-5 pt-16'+(right?'pr-10 pl-3':'pr-3 pl-10')" shaped id="card">
     <v-card class="circle" :style="(right?'right':'left')+': -3.5rem'" elevation="24">
       <v-row class="justify-center">
-        <v-icon size="40">{{icon}}</v-icon>
+        <v-icon color="white" size="40">{{icon}}</v-icon>
       </v-row>
       <v-row class="justify-center">
         <h5>{{title}}</h5>
@@ -33,11 +33,19 @@ export default {
   /*right: -3.5rem;*/
   top: -3.5rem;
   border-radius: 100% !important;
-  background: #ffeb3b;
+  background: #3b0bf9;
+  color: white;
   height: 7rem;
   width: 7rem;
   padding-top: 2rem;
   font-size: 1.5rem;
-
 }
+#card{
+  color: white;
+  background-image: url("../../assets/about-us.svg");
+  background-repeat: repeat;
+  background-position: center;
+  padding: 4em;
+}
+
 </style>
