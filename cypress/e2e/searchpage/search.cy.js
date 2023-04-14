67891a0b877199cb1a972f21b6a7c08ade55b2c5
Jasmine <jasminesr2001@gmail.com>
2023-04-14 21:11:39 +0430

    search test

--- cypress/e2e/searchpage/search.cy.js
+++ cypress/e2e/searchpage/search.cy.js
@@ -1,13 +1,15 @@
 const baseData = require('../../fixtures/shared.json')
+const mockData = require('../../fixtures/APIResults.json')
 describe('Search component', () => {
     beforeEach(() => {
-      cy.visit(baseData.host + '/signup')
+      cy.intercept('*/search/*', mockData.search)
+      cy.visit(baseData.host + '/search')
     })
     it('renders the component', () => {
-      cy.get('search-card').should('have.v-card', ':style', 'background-image: url(http://localhost:8000/storage/avatars/${image})')
-      cy.get('search-card').should('have.h2')
-      cy.get('search-card').should('have.p')
-      cy.get('search-card').should('have.prop')
+      // cy.get('.whole').should('have.v-card', ':style', 'background-image: url(http://localhost:8000/storage/avatars/${image})')
+      cy.get('.whole').should('have.h2')
+      cy.get('.whole').should('contain.text', mockData.search.data[0].name)
+      // cy.get('.whole').should('have.prop')
     })
   
 })
