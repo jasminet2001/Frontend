89bf9ab179f98b5899422b9fd122d86de4b30774
Jasmine <jasminesr2001@gmail.com>
2023-04-15 11:00:31 +0430

    one more test for search

--- cypress/e2e/searchpage/search.cy.js
+++ cypress/e2e/searchpage/search.cy.js
@@ -6,7 +6,7 @@ describe('Search component', () => {
       cy.visit(baseData.host + '/search')
     })
     it('renders the component', () => {
-      // cy.get('.whole').should('have.v-card', ':style', 'background-image: url(http://localhost:8000/storage/avatars/${image})')
+      cy.get('.whole').should('have.v-card', ':style', 'background-image: url(http://localhost:8000/storage/avatars/${image})')
       cy.get('.whole').should('have.h2')
       cy.get('.whole').should('contain.text', mockData.search.data[0].name)
       // cy.get('.whole').should('have.prop')
