3ca840826207f8a8805973b277d68b7ce22f5431
Jasmine <jasminesr2001@gmail.com>
2023-04-22 13:32:13 +0430

    creating css for adoverview

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import VueAxios from 'vue-axios'
 import VueRouter from 'vue-router'
 import Routes from './routes'
 import Store from './store'
-// import './assets/styles/global.scss';
+import './assets/styles/global.css'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
